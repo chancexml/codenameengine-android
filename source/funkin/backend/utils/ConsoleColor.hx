8f119a3ad4e368b14875d940b05ebcab4fa1cd58
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-24 17:34:59 -0700

    Delete source/funkin/backend/utils/ConsoleColor.hx

--- source/funkin/backend/utils/ConsoleColor.hx
+++
@@ -1,8 +0,0 @@
-package funkin.backend.utils;
-
-class ConsoleColor {
-    public static final GREEN:Int = 0xFF00FF00;
-    public static final RED:Int = 0xFFFF0000;
-    public static final YELLOW:Int = 0xFFFFFF00;
-    public static final WHITE:Int = 0xFFFFFFFF;
-}
