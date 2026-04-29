411a416d7ed17b432f6b5e5ad3d275b699570533
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-29 13:06:56 -0700

    new options

--- source/funkin/options/categories/AndroidOptions.hx
+++ source/funkin/options/categories/AndroidOptions.hx
@@ -4,16 +4,48 @@ import flixel.FlxG;
 import funkin.options.Options;
 import funkin.options.type.Checkbox;
 import funkin.options.type.SliderOption;
-
+/**
+public static var hitboxOpacity:Float = 0.15;
+public static var hitboxHints:Float = 0.25;
+public static var pauseButton:Bool = true;
+public static var hitboxStyle:String = "Simple";
+public static var hintStyle:String = "Simple";
+**/
 class AndroidOptions extends TreeMenuScreen {
 
     public function new() {
         super('optionsTree.android-name', 'optionsTree.android-desc', '');
 
         add(new Checkbox(
-            getNameID('hitboxHints'),
-            getDescID('hitboxHints'),
-            'hitboxHints'
+            getNameID('pauseButton'),
+            getDescID('pauseButton'),
+            'pauseButton'
+        ));
+
+        add(new ArrayOption(
+            getNameID('hintStyle'),
+            getDescID('hintStyle'),
+            ['Simple', 'Gradient'],
+            ['Simple', 'Gradient'],
+            'hinrStyle'
+        ));
+
+        add(new ArrayOption(
+            getNameID('hitboxStyle'),
+            getDescID('hitboxStyle'),
+            ['Simple', 'Gradient'],
+            ['Simple', 'Gradient'],
+            'hitboxStyle'
+        ));
+
+        add(new SliderOption(
+            getNameID('hintOpacity'),
+            getDescID('hintOpacity'),
+            0,
+            1,
+            0.05,
+            2,
+            'hintOpacity'
         ));
 
         add(new SliderOption(
@@ -22,7 +54,7 @@ class AndroidOptions extends TreeMenuScreen {
             0,
             1,
             0.05,
-            5,
+            2,
             'hitboxOpacity'
         ));
     }
