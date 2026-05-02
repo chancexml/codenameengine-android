10c597c804a4cf2b7ff755396da1ba61e8e093fc
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-02 10:30:39 -0700

    Update CustomShader.hx

--- source/funkin/backend/shaders/CustomShader.hx
+++ source/funkin/backend/shaders/CustomShader.hx
@@ -2,99 +2,39 @@ package funkin.backend.shaders;
 
 import openfl.Assets;
 
+/**
+ * Class for custom shaders.
+ *
+ * To create one, create a `shaders` folder in your assets/mod folder, then add a file named `my-shader.frag` or/and `my-shader.vert`.
+ *
+ * Non-existent shaders will only load the default one, and throw a warning in the console.
+ *
+ * To access the shader's uniform variables, use `shader.variable`
+ */
 class CustomShader extends FunkinShader {
 	public var path:String = "";
 
+	/**
+	 * Creates a new custom shader
+	 * @param name Name of the frag and vert files.
+	 * @param glslVersion GLSL version to use. Defaults to `120`.
+	 */
 	public function new(name:String, glslVersion:String = null) {
-		if (glslVersion == null)
-			glslVersion = Flags.DEFAULT_GLSL_VERSION;
-
+		if (glslVersion == null) glslVersion = Flags.DEFAULT_GLSL_VERSION;
 		var fragShaderPath = Paths.fragShader(name);
 		var vertShaderPath = Paths.vertShader(name);
-
-		var fragCode:String = Assets.exists(fragShaderPath)
-			? Assets.getText(fragShaderPath)
-			: null;
-
-		var vertCode:String = Assets.exists(vertShaderPath)
-			? Assets.getText(vertShaderPath)
-			: null;
+		var fragCode = Assets.exists(fragShaderPath) ? Assets.getText(fragShaderPath) : null;
+		var vertCode = Assets.exists(vertShaderPath) ? Assets.getText(vertShaderPath) : null;
 
 		fileName = name;
 		fragFileName = fragShaderPath;
 		vertFileName = vertShaderPath;
 
-		path = fragShaderPath;
+		path = fragShaderPath+vertShaderPath;
 
-		if (fragCode != null && fragCode.trim() != "")
-		{
-			fragCode = fixShader(fragCode, true);
-		}
-
-		if (vertCode != null && vertCode.trim() != "")
-		{
-			vertCode = fixShader(vertCode, false);
-		}
-
-		if ((fragCode == null || fragCode.trim() == "")
-			&& (vertCode == null || vertCode.trim() == ""))
-		{
+		if (fragCode == null && vertCode == null)
 			Logs.error('Shader "$name" couldn\'t be found.');
-		}
 
 		super(fragCode, vertCode, glslVersion);
 	}
-
-	private function fixShader(code:String, isFragment:Bool):String
-{
-	code = StringTools.replace(code, "\r\n", "\n");
-
-	if (!code.contains("#pragma header"))
-	{
-		code = "#pragma header\n\n" + code;
-	}
-
-	if (!code.contains("precision mediump float"))
-	{
-		code =
-			"#ifdef GL_ES\n" +
-			"precision mediump float;\n" +
-			"#endif\n\n" +
-			code;
-	}
-
-	code = StringTools.replace(
-		code,
-		"texture2D(",
-		"flixel_texture2D("
-	);
-
-	code = StringTools.replace(
-		code,
-		"texture(",
-		"flixel_texture2D("
-	);
-
-	code = StringTools.replace(
-		code,
-		"openfl_TextureCoordv.xy",
-		"clamp(openfl_TextureCoordv, 0.001, 0.999)"
-	);
-
-	code = StringTools.replace(
-		code,
-		"openfl_TextureCoordv",
-		"clamp(openfl_TextureCoordv, 0.001, 0.999)"
-	);
-	
-	if (!code.contains("void main"))
-	{
-		if (isFragment)
-			code += "\nvoid main(){ gl_FragColor = vec4(1.0); }";
-		else
-			code += "\nvoid main(){ }";
-	}
-
-	return code;
-    }
 }
