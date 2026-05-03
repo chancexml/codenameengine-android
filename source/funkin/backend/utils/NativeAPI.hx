68c5496ca7bdd54c25d0b4fed300a7680d60077d
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-02 20:12:12 -0700

    use adaptive native api

--- source/funkin/backend/utils/NativeAPI.hx
+++ source/funkin/backend/utils/NativeAPI.hx
@@ -1,200 +1,163 @@
-package funkin.backend.utils;
+package backend;
 
-import haxe.macro.Expr.Case;
 import openfl.ui.Mouse;
 import openfl.ui.MouseCursor;
-import funkin.backend.utils.native.*;
 import flixel.util.typeLimit.OneOfTwo;
 import flixel.util.typeLimit.OneOfThree;
 import flixel.util.FlxColor;
 
-/**
- * Class for functions that talk to a lower level than haxe, such as message boxes, and more.
- * Some functions might not have effect on some platforms.
- */
+#if android
+import extension.androidtools.Tools;
+#end
+
+#if (windows && !macro)
+@:cppFileCode('
+#include <windows.h>
+#include <iostream>
+#include <dwmapi.h>
+#pragma comment(lib, "Dwmapi.lib")
+')
+#end
 class NativeAPI {
-	@:dox(hide) public static function registerAudio() {
-		#if windows
-		Windows.registerAudio();
-		#end
-	}
-
-	@:dox(hide) public static function registerAsDPICompatible() {
-		#if windows
-		Windows.registerAsDPICompatible();
-		#end
-	}
-
-	/**
-	 * Allocates a new console. The console will automatically be opened
-	 */
+	
 	public static function allocConsole() {
-		#if windows
-		Windows.allocConsole();
-		Windows.clearScreen();
+		#if (windows && !macro)
+		untyped __cpp__('
+			AllocConsole();
+			freopen("CONIN$", "r", stdin);
+			freopen("CONOUT$", "w", stdout);
+			freopen("CONOUT$", "w", stderr);
+		');
+		#elseif android
+		// not doing it..
 		#end
 	}
 
-	/**
-	 * Gets the specified file's (or folder) attributes.
-	 */
 	public static function getFileAttributesRaw(path:String, useAbsolute:Bool = true):Int {
-		#if windows
-		if(useAbsolute) path = sys.FileSystem.absolutePath(path);
-		return Windows.getFileAttributes(path);
+		#if (windows && !macro)
+		return untyped __cpp__('GetFileAttributesA({0}.c_str())', path);
 		#else
 		return -1;
 		#end
 	}
 
-	/**
-	 * Gets the specified file's (or folder) attributes and passes it to `FileAttributeWrapper`.
-	 */
 	public static function getFileAttributes(path:String, useAbsolute:Bool = true):FileAttributeWrapper {
 		return new FileAttributeWrapper(getFileAttributesRaw(path, useAbsolute));
 	}
 
-	/**
-	 * Sets the specified file's (or folder) attributes. If it fails, the return value is `0`.
-	 */
 	public static function setFileAttributes(path:String, attrib:OneOfThree<NativeAPI.FileAttribute, FileAttributeWrapper, Int>, useAbsolute:Bool = true):Int {
-		#if windows
-		if(useAbsolute) path = sys.FileSystem.absolutePath(path);
-		return Windows.setFileAttributes(path, attrib is FileAttributeWrapper ? cast(attrib, FileAttributeWrapper).getValue() : cast(attrib, Int));
+		#if (windows && !macro)
+		var attrInt:Int = cast(attrib, Int);
+		return untyped __cpp__('SetFileAttributesA({0}.c_str(), {1})', path, attrInt) ? 1 : 0;
 		#else
 		return 0;
 		#end
 	}
 
-	/**
-	 * Removes from the specified file's (or folder) one (or more) specific attribute.
-	 */
 	public static function addFileAttributes(path:String, attrib:OneOfTwo<NativeAPI.FileAttribute, Int>, useAbsolute:Bool = true):Int {
-		#if windows
+		#if (windows && !macro)
 		return setFileAttributes(path, getFileAttributesRaw(path, useAbsolute) | cast(attrib, Int), useAbsolute);
 		#else
 		return 0;
 		#end
 	}
 
-	/**
-	 * Removes from the specified file's (or folder) one (or more) specific attribute.
-	 */
 	public static function removeFileAttributes(path:String, attrib:OneOfTwo<NativeAPI.FileAttribute, Int>, useAbsolute:Bool = true):Int {
-		#if windows
+		#if (windows && !macro)
 		return setFileAttributes(path, getFileAttributesRaw(path, useAbsolute) & ~cast(attrib, Int), useAbsolute);
 		#else
 		return 0;
 		#end
 	}
 
-	/**
-	 * WINDOW COLOR MODE FUNCTIONS.
-	 */
-
-	/**
-	 * Switch the window's color mode to dark or light mode.
-	 */
 	public static function setDarkMode(title:String, enable:Bool) {
-		#if windows
-		if(title == null) title = lime.app.Application.current.window.title;
-		Windows.setDarkMode(title, enable);
+		#if (windows && !macro)
+		var val:Int = enable ? 1 : 0;
+		untyped __cpp__('
+			HWND window = GetActiveWindow();
+			int darkVal = {0};
+			DwmSetWindowAttribute(window, 20, &darkVal, sizeof(darkVal));
+		', val);
 		#end
 	}
 
-	/**
-	 * Switch the window's color to any color.
-	 *
-	 * WARNING: This is exclusive to windows 11 users, unfortunately.
-	 *
-	 * NOTE: Setting the color to 0x00000000 (FlxColor.TRANSPARENT) will set the border (must have setBorder on) invisible.
-	 */
 	public static function setWindowBorderColor(title:String, color:FlxColor, setHeader:Bool = true, setBorder:Bool = true) {
-		#if windows
-		if(title == null) title = lime.app.Application.current.window.title;
-		Windows.setWindowBorderColor(title, [color.red, color.green, color.blue, color.alpha], setHeader, setBorder);
+		#if (windows && !macro)
+		var c:Int = (color.b << 16) | (color.g << 8) | color.r;
+		untyped __cpp__('
+			HWND window = GetActiveWindow();
+			int colorVal = {0};
+			if ({1}) DwmSetWindowAttribute(window, 35, &colorVal, sizeof(colorVal));
+			if ({2}) DwmSetWindowAttribute(window, 34, &colorVal, sizeof(colorVal));
+		', c, setHeader, setBorder);
 		#end
 	}
 
-	/**
-	 * Resets the window's border color to the default one.
-	 *
-	 * WARNING: This is exclusive to windows 11 users, unfortunately.
-	**/
 	public static function resetWindowBorderColor(title:String, setHeader:Bool = true, setBorder:Bool = true) {
-		#if windows
-		if(title == null) title = lime.app.Application.current.window.title;
-		Windows.setWindowBorderColor(title, [-1, -1, -1, -1], setHeader, setBorder);
+		#if (windows && !macro)
+		untyped __cpp__('
+			HWND window = GetActiveWindow();
+			int defaultColor = 0xFFFFFFFF;
+			if ({0}) DwmSetWindowAttribute(window, 35, &defaultColor, sizeof(defaultColor));
+			if ({1}) DwmSetWindowAttribute(window, 34, &defaultColor, sizeof(defaultColor));
+		', setHeader, setBorder);
 		#end
 	}
 
-	/**
-	 * Switch the window's title text to any color.
-	 *
-	 * WARNING: This is exclusive to windows 11 users, unfortunately.
-	 */
 	public static function setWindowTitleColor(title:String, color:FlxColor) {
-		#if windows
-		if(title == null) title = lime.app.Application.current.window.title;
-		Windows.setWindowTitleColor(title, [color.red, color.green, color.blue, color.alpha]);
+		#if (windows && !macro)
+		var c:Int = (color.b << 16) | (color.g << 8) | color.r; 
+		untyped __cpp__('
+			HWND window = GetActiveWindow();
+			int colorVal = {0};
+			DwmSetWindowAttribute(window, 36, &colorVal, sizeof(colorVal));
+		', c);
 		#end
 	}
 
-	/**
-	 * Resets the window's title color to the default one.
-	 *
-	 * WARNING: This is exclusive to windows 11 users, unfortunately.
-	**/
 	public static function resetWindowTitleColor(title:String) {
-		#if windows
-		if(title == null) title = lime.app.Application.current.window.title;
-		Windows.setWindowTitleColor(title, [-1, -1, -1, -1]);
+		#if (windows && !macro)
+		untyped __cpp__('
+			HWND window = GetActiveWindow();
+			int defaultColor = 0xFFFFFFFF; 
+			DwmSetWindowAttribute(window, 36, &defaultColor, sizeof(defaultColor)); 
+		');
 		#end
 	}
 
-	/**
-	 * Forces the window header to redraw, causes a small visual jitter so use it sparingly.
-	 */
 	public static function redrawWindowHeader() {
-		#if windows
+		#if desktop
 		flixel.FlxG.stage.window.borderless = true;
 		flixel.FlxG.stage.window.borderless = false;
 		#end
 	}
 
-	/**
-	 * Can be used to check if your using a specific version of an OS (or if your using a certain OS).
-	 */
 	public static function hasVersion(vers:String)
 		return lime.system.System.platformLabel.toLowerCase().indexOf(vers.toLowerCase()) != -1;
 
-	/**
-	 * Shows a message box
-	 */
-	public static function showMessageBox(caption:String, message:String, icon:MessageBoxIcon = MSG_WARNING) {
-		#if windows
-		Windows.showMessageBox(caption, message, icon);
+	public static function showMessageBox(caption:String, message:String, buttonName:String = "OK", icon:MessageBoxIcon = MSG_WARNING)
+{
+    #if android
+    extension.androidtools.Tools.showAlertDialog(caption, message, {name: buttonName, func: null});
+    #elseif (windows && !macro)
+    var iconInt:Int = cast(icon, Int);
+    untyped __cpp__('MessageBoxA(GetActiveWindow(), {0}.c_str(), {1}.c_str(), {2})', message, caption, iconInt);
+    #else
+    lime.app.Application.current.window.alert(message, caption);
+    #end
+}
+	
+	public static function showToast(message:String)
+	{
+		#if android
+		//extension.androidtools.Tools.showToast(message);
 		#else
-		lime.app.Application.current.window.alert(message, caption);
+		//trace("Toast: " + message);
 		#end
 	}
 
-	/**
-	 * Sets the console colors
-	 */
 	public static function setConsoleColors(foregroundColor:ConsoleColor = NONE, ?backgroundColor:ConsoleColor = NONE) {
-		if(Main.noTerminalColor) return;
-
-		#if (windows && !hl)
-		if(foregroundColor == NONE)
-			foregroundColor = LIGHTGRAY;
-		if(backgroundColor == NONE)
-			backgroundColor = BLACK;
-
-		var fg:Int = cast foregroundColor;
-		var bg:Int = cast backgroundColor;
-		Windows.setConsoleColors((bg * 16) + fg);
-		#elseif sys
+		#if (sys && !android)
 		Sys.print("\x1b[0m");
 		if(foregroundColor != NONE)
 			Sys.print("\x1b[" + Std.int(consoleColorToANSI(foregroundColor)) + "m");
@@ -203,13 +166,8 @@ class NativeAPI {
 		#end
 	}
 
-	/**
-	 * Set cursor icon.
-	**/
 	public static function setCursorIcon(icon:CodeCursor) {
-		#if (mac && cpp)
-		Mac.setMouseCursorIcon(cast icon);
-		#else
+		#if desktop
 		Mouse.cursor = icon.toOpenFL();
 		#end
 	}
@@ -257,8 +215,17 @@ class NativeAPI {
 	}
 }
 
+class FileAttributeWrapper {
+	var value:Int;
+	public function new(val:Int) {
+		this.value = val;
+	}
+	public function getValue():Int {
+		return value;
+	}
+}
+
 enum abstract FileAttribute(Int) from Int to Int {
-	// Settables
 	var ARCHIVE = 0x20;
 	var HIDDEN = 0x2;
 	var NORMAL = 0x80;
@@ -267,8 +234,6 @@ enum abstract FileAttribute(Int) from Int to Int {
 	var READONLY = 0x1;
 	var SYSTEM = 0x4;
 	var TEMPORARY = 0x100;
-
-	// Non Settables
 	var COMPRESSED = 0x800;
 	var DEVICE = 0x40;
 	var DIRECTORY = 0x10;
@@ -294,7 +259,6 @@ enum abstract ConsoleColor(Int) {
 	var MAGENTA = 13;
 	var YELLOW = 14;
 	var WHITE = 15;
-
 	var NONE = -1;
 }
 
@@ -306,33 +270,30 @@ enum abstract MessageBoxIcon(Int) {
 }
 
 enum abstract CodeCursor(String) {
-	var CUSTOM;// = "arrow";
-	var ARROW;// = "arrow";
-	var CLICK;// = "click";
-	var CROSSHAIR;// = "crosshair";
-	var HAND;// = "hand";
-	var IBEAM;// = "ibeam";
-	var MOVE;// = "move";
-
-	var RESIZE_H;// = "resize_we";
-	var RESIZE_V;// = "resize_ns";
-	var RESIZE_TL;// = "resize_nw";
-	var RESIZE_TR;// = "resize_ne";
-	var RESIZE_BL;// = "resize_sw";
-	var RESIZE_BR;// = "resize_se";
-	var RESIZE_T;// = "resize_n";
-	var RESIZE_B;// = "resize_b";
-	var RESIZE_L;// = "resize_w";
-	var RESIZE_R;// = "resize_e";
-
-	var RESIZE_TLBR;// = "resize_nw_se";
-	var RESIZE_TRBL;// = "resize_ne_sw";
-
-	var WAIT;// = "wait";
-	var WAIT_ARROW;// = "waitarrow";
-	var DISABLED;// = "disabled";
-	var DRAG;// = "drag";
-	var DRAG_OPEN;// = "dragopen";
+	var CUSTOM;
+	var ARROW;
+	var CLICK;
+	var CROSSHAIR;
+	var HAND;
+	var IBEAM;
+	var MOVE;
+	var RESIZE_H;
+	var RESIZE_V;
+	var RESIZE_TL;
+	var RESIZE_TR;
+	var RESIZE_BL;
+	var RESIZE_BR;
+	var RESIZE_T;
+	var RESIZE_B;
+	var RESIZE_L;
+	var RESIZE_R;
+	var RESIZE_TLBR;
+	var RESIZE_TRBL;
+	var WAIT;
+	var WAIT_ARROW;
+	var DISABLED;
+	var DRAG;
+	var DRAG_OPEN;
 
 	@:to public function toOpenFL():MouseCursor {
 		return @:privateAccess switch(cast this) {
@@ -343,28 +304,23 @@ enum abstract CodeCursor(String) {
 			case MOVE: MouseCursor.__MOVE;
 			case HAND: MouseCursor.HAND;
 			case DRAG: MouseCursor.HAND;
-			case DRAG_OPEN: MouseCursor.ARROW; // Could be HAND, but it might be better to use ARROW since on windows it would be weird to have the dragging cursor
+			case DRAG_OPEN: MouseCursor.ARROW; 
 			case WAIT: MouseCursor.__WAIT;
 			case WAIT_ARROW: MouseCursor.__WAIT_ARROW;
-
 			case DISABLED: MouseCursor.ARROW;
-
 			case RESIZE_TR: MouseCursor.__RESIZE_NESW;
 			case RESIZE_BL: MouseCursor.__RESIZE_NESW;
 			case RESIZE_TL: MouseCursor.__RESIZE_NWSE;
 			case RESIZE_BR: MouseCursor.__RESIZE_NWSE;
 			case RESIZE_H: MouseCursor.__RESIZE_WE;
 			case RESIZE_V: MouseCursor.__RESIZE_NS;
-
 			case RESIZE_T: MouseCursor.__RESIZE_NS;
 			case RESIZE_B: MouseCursor.__RESIZE_NS;
 			case RESIZE_L: MouseCursor.__RESIZE_WE;
 			case RESIZE_R: MouseCursor.__RESIZE_WE;
-
 			case RESIZE_TLBR: MouseCursor.__RESIZE_NWSE;
 			case RESIZE_TRBL: MouseCursor.__RESIZE_NESW;
 			case CUSTOM: MouseCursor.__CUSTOM;
-			//default: ARROW;
 		}
 	}
 
@@ -380,24 +336,19 @@ enum abstract CodeCursor(String) {
 			case DRAG_OPEN: 7;
 			case WAIT: 8;
 			case WAIT_ARROW: 9;
-
 			case DISABLED: 10;
-
 			case RESIZE_TR: 11;
 			case RESIZE_BL: 12;
 			case RESIZE_TL: 13;
 			case RESIZE_BR: 14;
 			case RESIZE_H: 15;
 			case RESIZE_V: 16;
-
 			case RESIZE_T: 17;
 			case RESIZE_B: 18;
 			case RESIZE_L: 19;
 			case RESIZE_R: 20;
-
 			case RESIZE_TLBR: 21;
 			case RESIZE_TRBL: 22;
-
 			case CUSTOM: -1;
 		}
 	}
