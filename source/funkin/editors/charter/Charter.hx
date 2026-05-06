c90386474a16abe3ec9415680a908b74c2eb351d
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-05 19:47:44 -0700

    added missing thing in charter

--- source/funkin/editors/charter/Charter.hx
+++ source/funkin/editors/charter/Charter.hx
@@ -589,7 +589,7 @@ class Charter extends UIState {
         
         virtualPad = ButtonHelper.create(this, FULL, A_B_C_X_Y);
 
-        ButtonHelper.bind(virtualPad,['up','down','left','right']['accept','back','switchmod','pause','reset']);
+        ButtonHelper.bind(virtualPad,['up','down','left','right'],['accept','back','switchmod','pause','reset']);
         Controls.virtualPad = virtualPad;
         #end
 
