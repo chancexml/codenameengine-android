f74d787bffed385ddf781243e8dafc3a7c779b09
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-06 14:59:17 -0700

    OH YEAH!

--- source/funkin/editors/charter/Charter.hx
+++ source/funkin/editors/charter/Charter.hx
@@ -608,12 +608,15 @@ class Charter extends UIState {
 		DiscordUtil.call("onEditorLoaded", ["Chart Editor", __song + " (" + __diff + ")" + (__variant != null && __variant != "" ? " (" + __variant + ")" : "")]);
 
 		#if mobile
-		Call.Mouse();
         
         virtualPad = ButtonHelper.create(this, FULL, A_B_C_X_Y);
 
         ButtonHelper.bind(virtualPad,['up','down','left','right'],['accept','back','switchmod','pause','reset']);
         Controls.virtualPad = virtualPad;
+
+		Call.Mouse();
+
+		if (Call.mouse != null) Call.mouse.disableGhostTouch = true;
         #end
 	}
 
