818c91d91dbdf4a96d487c8dcfb89305986b3c54
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-05 19:21:55 -0700

    eh

--- source/funkin/backend/scripting/HScript.hx
+++ source/funkin/backend/scripting/HScript.hx
@@ -67,7 +67,7 @@ class HScript extends Script {
 		interp.variables.set("Controls", Controls);
 
 		#if mobile
-		interp.variables.set("Call", Call);
+		interp.variables.set("Call", virt.Call);
 		interp.variables.set("ButtonHelper", ButtonHelper);
 		interp.variables.set("VirtualPad", VirtualPad);
 		#end
